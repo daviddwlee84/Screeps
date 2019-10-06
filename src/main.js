58d026877f0d0f1c6ccb8559e3dcba08f01e52da
David Lee <daviddwlee84@gmail.com>
2019-10-06 22:06:11 +0800

    Seperate the harvester logic from main

--- src/main.js
+++ src/main.js
@@ -1,22 +1,15 @@
-// Initial version of basic harvesting energy
+// Move harvesting logic to role.harvester
 // Require manually create creeps in concole
 // Game.spawns['Spawn1'].spawnCreep( [WORK, CARRY, MOVE], 'Harvester1' );
 // Game.spawns['Spawn1'].spawnCreep( [WORK, CARRY, MOVE], 'Harvester2' );
 // ...
 
+var roleHarvester = require('role.harvester');
+
 module.exports.loop = function () {
+
     for (var name in Game.creeps) {
         var creep = Game.creeps[name];
-
-        if (creep.carry.energy < creep.carryCapacity) {
-            var sources = creep.room.find(FIND_SOURCES);
-            if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0]);
-            }
-        } else {
-            if (creep.transfer(Game.spawns['Spawn1'], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(Game.spawns['Spawn1']);
-            }
-        }
+        roleHarvester.run(creep);
     }
 }
