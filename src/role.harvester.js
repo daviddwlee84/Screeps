f3927d64b89453cd7a2668c6709a19b93bb0e407
David Lee <daviddwlee84@gmail.com>
2019-10-07 00:47:45 +0800

    Add visualize path style to workers

--- src/role.harvester.js
+++ src/role.harvester.js
@@ -11,11 +11,19 @@ var roleHarvester = {
         if (creep.carry.energy < creep.carryCapacity) {
             var sources = creep.room.find(FIND_SOURCES);
             if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0]);
+                creep.moveTo(sources[0], {
+                    visualizePathStyle: {
+                        stroke: '#ffaa00'
+                    }
+                });
             }
         } else {
             if (creep.transfer(Game.spawns['Spawn1'], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(Game.spawns['Spawn1']);
+                creep.moveTo(Game.spawns['Spawn1'], {
+                    visualizePathStyle: {
+                        stroke: '#ffffff'
+                    }
+                });
             }
         }
     }
--- src/role.upgrader.js
+++ src/role.upgrader.js
@@ -11,11 +11,19 @@ var roleUpgrader = {
         if (creep.carry.energy == 0) {
             var sources = creep.room.find(FIND_SOURCES);
             if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0]);
+                creep.moveTo(sources[0], {
+                    visualizePathStyle: {
+                        stroke: '#ffaa00'
+                    }
+                });
             }
         } else {
             if (creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(creep.room.controller);
+                creep.moveTo(creep.room.controller, {
+                    visualizePathStyle: {
+                        stroke: '#ffffff'
+                    }
+                });
             }
         }
     }
