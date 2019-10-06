7566a3e17bbafeebfba6891ec459a0c3691022c2
David Lee <daviddwlee84@gmail.com>
2019-10-06 21:27:56 +0800

    Basic harvesting logic

---
+++ src/main.js
@@ -0,0 +1,22 @@
+// Initial version of basic harvesting energy
+// Require manually create creeps in concole
+// Game.spawns['Spawn1'].spawnCreep( [WORK, CARRY, MOVE], 'Harvester1' );
+// Game.spawns['Spawn1'].spawnCreep( [WORK, CARRY, MOVE], 'Harvester2' );
+// ...
+
+module.exports.loop = function () {
+    for (var name in Game.creeps) {
+        var creep = Game.creeps[name];
+
+        if (creep.carry.energy < creep.carryCapacity) {
+            var sources = creep.room.find(FIND_SOURCES);
+            if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[0]);
+            }
+        } else {
+            if (creep.transfer(Game.spawns['Spawn1'], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(Game.spawns['Spawn1']);
+            }
+        }
+    }
+}
