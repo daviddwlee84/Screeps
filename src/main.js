f1705c8abafd4544cbf711bcd81851e0bbdd658b
David Lee <daviddwlee84@gmail.com>
2019-10-07 12:58:59 +0800

    Add builder and upgrade harvester for extension

--- src/main.js
+++ src/main.js
@@ -1,19 +1,30 @@
 // Move harvesting logic to role.harvester
 // Require manually create creeps in concole
 // Harvester
-// Game.spawns['Spawn1'].spawnCreep( [WORK, CARRY, MOVE], 'Harvester1' );
+// Game.spawns['Spawn1'].spawnCreep( [WORK, CARRY, MOVE], 'Harvester1', { memory: { role: 'harvester' } } );
+// Game.spawns['Spawn1'].spawnCreep( [WORK,WORK,WORK,WORK,CARRY,MOVE,MOVE], 'HarvesterBig', { memory: { role: 'harvester' } } );
 // ...
 // Upgrader
-// Game.spawns['Spawn1'].spawnCreep( [WORK, CARRY, MOVE], 'Upgrader1' );
+// Game.spawns['Spawn1'].spawnCreep( [WORK, CARRY, MOVE], 'Upgrader1', { memory: { role: 'upgrader' } } );
 // ...
-// Set Memory
-// Game.creeps['Harvester1'].memory.role = 'harvester';
-// Game.creeps['Upgrader1'].memory.role = 'upgrader';
+// Builder
+// Game.spawns['Spawn1'].spawnCreep( [WORK, CARRY, MOVE], 'Builder1', { memory: { role: 'builder' } } );
+// Set Role Memory
+// Games.creeps['CreepsName'].memory.role = 'roleType'
+// Build Structure
+// Build Extension
+// Game.rooms['W25S17'].createConstructionSite(32, 36, STRUCTURE_EXTENSION, ['Extension1']) 
 
 var roleHarvester = require('role.harvester');
 var roleUpgrader = require('role.upgrader');
+var roleBuilder = require('role.builder')
 
 module.exports.loop = function () {
+
+    for (var name in Game.rooms) {
+        // Showing the current energy that we have
+        console.log('Room "' + name + '" has ' + Game.rooms[name].energyAvailable + ' energy');
+    }
 
     for (var name in Game.creeps) {
         var creep = Game.creeps[name];
@@ -22,6 +33,9 @@ module.exports.loop = function () {
         }
         if (creep.memory.role == 'upgrader') {
             roleUpgrader.run(creep);
+        }
+        if (creep.memory.role == 'builder') {
+            roleBuilder.run(creep);
         }
     }
 }
--- src/role.harvester.js
+++ src/role.harvester.js
@@ -18,12 +18,20 @@ var roleHarvester = {
                 });
             }
         } else {
-            if (creep.transfer(Game.spawns['Spawn1'], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(Game.spawns['Spawn1'], {
-                    visualizePathStyle: {
-                        stroke: '#ffffff'
-                    }
-                });
+            var targets = creep.room.find(FIND_STRUCTURES, {
+                filter: (structure) => {
+                    return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
+                        structure.energy < structure.energyCapacity;
+                }
+            });
+            if (targets.length > 0) {
+                if (creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targets[0], {
+                        visualizePathStyle: {
+                            stroke: '#ffffff'
+                        }
+                    });
+                }
             }
         }
     }
