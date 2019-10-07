8a2d6c770cc8dd4ed2e1e14173153759f3a7cd5b
David Lee <daviddwlee84@gmail.com>
2019-10-07 16:24:42 +0800

    Add repair ability to builder

--- src/role.builder.js
+++ src/role.builder.js
@@ -1,8 +1,9 @@
 /*
  * Role of Builder
- * 1. If not carrying energy or building then go harvest
- * 2. If the carry capacity is full then go building (switch to building mode)
+ * 1. If not carrying energy or building or reparing then go harvest
+ * 2. If the carry capacity is full then go building (switch to building mode) or go reparing (switch to reparining mode)
  * 3. If in building mode then go find a construction site
+ * 3. If in reparing mode then go find a closest damaged structure
  * 4. If in harvest mode then go find a energy source
  */
 
@@ -11,13 +12,19 @@ var roleBuilder = {
     /** @param {Creep} creep **/
     run: function (creep) {
 
-        if (creep.memory.building && creep.carry.energy == 0) {
+        if ((creep.memory.building || creep.memory.reparing) && creep.carry.energy == 0) {
             creep.memory.building = false;
+            creep.memory.reparing = false;
             creep.say('🔄 harvest');
         }
-        if (!creep.memory.building && creep.carry.energy == creep.carryCapacity) {
-            creep.memory.building = true;
-            creep.say('🚧 build');
+
+        // TODO: when switch role they may have a status that creep.carry.energy is either 0 or full
+
+        if ((!creep.memory.building && !creep.memory.reparing) && creep.carry.energy == creep.carryCapacity) {
+            var toBuild = Math.random() < 0.5;
+            creep.memory.building = toBuild;
+            creep.memory.reparing = !toBuild;
+            toBuild ? creep.say('🚧 build') : creep.say('🔧 repair');
         }
 
         if (creep.memory.building) {
@@ -30,6 +37,17 @@ var roleBuilder = {
                         }
                     });
                 }
+            } else {
+                // Nothing to build, go to repair
+                creep.memory.building = false;
+                creep.memory.reparing = true;
+            }
+        } else if (creep.memory.reparing) {
+            var closestDamagedStructure = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+                filter: (structure) => structure.hits < structure.hitsMax
+            });
+            if (closestDamagedStructure) {
+                creep.repair(closestDamagedStructure);
             }
         } else {
             var sources = creep.room.find(FIND_SOURCES);
