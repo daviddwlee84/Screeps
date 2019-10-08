8614f31b5ba319db2c4dad19f6edada62a79bb98
David Lee <daviddwlee84@gmail.com>
2019-10-08 11:49:44 +0800

    spawn larger minion (hard coded)

--- src/autospawn.js
+++ src/autospawn.js
@@ -1,3 +1,12 @@
+// Auto-spawn creeps
+
+/**
+ * TODO: if use larger one harvester, then if there are no energy in the extensions
+ * because the maximum capacity of spawn is 300
+ * (and it will regenerate 1 energy per tick but won't share with extension)
+ * so there is a chance that a harvester can't be built
+ */
+
 var capitalizeFirstLetter = require('utils')
 
 var countAndRespawn = function (roleType, count) {
@@ -6,7 +15,9 @@ var countAndRespawn = function (roleType, count) {
     console.log(workerName + 's: ' + workers.length);
 
     // Currently the "[WORK, CARRY, MOVE]" will need 200 energy
-    if (Game.spawns['Spawn1'].room.energyAvailable < 200) {
+    // Currently the "[WORK, WORK, CARRY, MOVE]" will need 300 energy
+    // Currently the "[WORK, WORK, WORK, WORK, CARRY, MOVE, MOVE]" will need 550 energy
+    if (Game.spawns['Spawn1'].room.energyAvailable < 300) {
         // Not enough energy
         return false;
     }
@@ -14,7 +25,7 @@ var countAndRespawn = function (roleType, count) {
     if (workers.length < count) {
         var newName = workerName + Game.time;
         console.log('Spawning new harvester: ' + newName);
-        Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, MOVE], newName, {
+        Game.spawns['Spawn1'].spawnCreep([WORK, WORK, CARRY, MOVE], newName, {
             memory: {
                 role: roleType
             }
