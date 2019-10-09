68a3cf180f44dc48120629d5ff6298bcc20fd518
David Lee <daviddwlee84@gmail.com>
2019-10-09 21:06:26 +0800

    Add remote worker template for later use

--- src/template.role.js
+++ src/template.role.js
@@ -1,6 +1,6 @@
 module.exports = {
     creepTemplate: {
-        'worker': {
+        'worker': { // Work in base, just care about efficiency
             'Little': { // The most basic one with all the necessary abilities
                 requiredEnergy: 200,
                 bodyContent: [WORK, CARRY, MOVE]
@@ -19,9 +19,29 @@ module.exports = {
             },
             'Large': { // Need energy of entire spawn and 20 full extensions (RCL 4)
                 requiredEnergy: 1300,
-                bodyContent: [WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK,
-                    CARRY, CARRY, CARRY, CARRY,
+                bodyContent: [WORK, WORK, WORK, WORK, WORK, WORK,
+                    CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY,
                     MOVE, MOVE, MOVE, MOVE
+                ]
+            },
+        },
+        'remoteWorker': { // Work across rooms/maps
+            // i.e. The remote harvester
+            'Carrier': { // Need energy of entire spawn and 10 full extensions (RCL 3)
+                requiredEnergy: 800,
+                bodyContent: [WORK, WORK,
+                    CARRY, CARRY, CARRY, CARRY, CARRY, CARRY,
+                    MOVE, MOVE, MOVE, MOVE,
+                    TOUGH, TOUGH, TOUGH, TOUGH, TOUGH, TOUGH, TOUGH, TOUGH, TOUGH, TOUGH
+                ]
+            },
+            // Collect the dropped energy?!
+            'Collector': { // Need energy of entire spawn and 10 full extensions (RCL 3)
+                requiredEnergy: 800,
+                bodyContent: [WORK,
+                    CARRY, CARRY, CARRY, CARRY,
+                    MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE,
+                    TOUGH, TOUGH, TOUGH, TOUGH, TOUGH, TOUGH, TOUGH, TOUGH, TOUGH, TOUGH
                 ]
             },
         }
