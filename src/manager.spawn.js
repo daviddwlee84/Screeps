7c7f28df613af89d82147aa1c8bb79ce99a5d65a
David Lee <daviddwlee84@gmail.com>
2019-10-12 11:30:30 +0800

    Remove autospawn.js and move functions to manager

--- src/manager.spawn.js
+++ src/manager.spawn.js
@@ -1,20 +1,52 @@
-var RCL_CONFIG = require('config.rcl')
-var capitalizeFirstLetter = require('utils')
-var spawnCommander = require('autospawn')
+var RCL_CONFIG = require('config.rcl');
+var capitalizeFirstLetter = require('utils');
+var creepTemplate = require('template.role').creepTemplate;
+var roleTypeToJob = require('template.role').roleTypeToJob;
 
-
-var countWorker = function (roleType, total) {
+function countWorker(roleType, total) {
     // Now assume all the same roleType will use same creepSize
     var workers = _.filter(Game.creeps, (creep) => creep.memory.role == roleType);
-    var workerName = capitalizeFirstLetter(roleType)
+    var workerName = capitalizeFirstLetter(roleType);
     console.log(`${workerName}s: ${workers.length}/${total}`);
-    return workers.length
+    return workers.length;
+}
+
+function respawnRole(spawn, roleType, creepSize) {
+    // Get specific creep type by its role and size
+    var creepType = creepTemplate[roleTypeToJob[roleType]][creepSize];
+    if (spawn.room.energyAvailable < creepType.requiredEnergy) {
+        // Not enough energy
+        return ERR_NOT_ENOUGH_ENERGY;
+    }
+    var workerName = capitalizeFirstLetter(roleType)
+    var newName = workerName + creepSize + Game.time;
+    console.log('Spawning new harvester: ' + newName);
+    return spawn.spawnCreep(creepType.bodyContent, newName, {
+        memory: {
+            role: roleType,
+            size: creepSize,
+            idleCount: 0
+        }
+    });
+}
+
+function showSpawnInfo(spawn) {
+    var spawningCreep = Game.creeps[spawn.spawning.name];
+    var progress = ((1 - spawn.spawning.remainingTime / spawn.spawning.needTime) * 100).toFixed();
+    spawn.room.visual.text(
+        '🛠️' + spawningCreep.memory.role + ` (${progress}%)`,
+        spawn.pos.x + 1,
+        spawn.pos.y, {
+            align: 'left',
+            opacity: 0.8
+        });
 }
 
 module.exports = {
     main: function (spawn) {
-        RCLLevel = spawn.room.controller.level;
-        toSpawn = RCL_CONFIG[RCLLevel].Spawn;
+        // Spawn creeps based on RCL (room controller level)
+        RCL = spawn.room.controller.level;
+        toSpawn = RCL_CONFIG[RCL].Spawn;
         for (var roleType in toSpawn) {
             var amount = countWorker(roleType, toSpawn[roleType].minAmount);
             toSpawn[roleType].count = amount;
@@ -24,17 +56,17 @@ module.exports = {
             // Respawn with priority
             for (var roleType in toSpawn) {
                 if (toSpawn[roleType].count < toSpawn[roleType].minAmount) {
-                    var ret = spawnCommander.respawnRole(spawn, roleType, toSpawn[roleType].creepSize);
+                    var ret = respawnRole(spawn, roleType, toSpawn[roleType].creepSize);
                     if (ret == OK) {
                         break;
                     } else if (ret == ERR_NOT_ENOUGH_ENERGY) {
-                        console.log(`Waiting for more energy (${roleType} pending in room ${spawn.room.name})`)
+                        console.log(`Waiting for more energy (${roleType} pending in room ${spawn.room.name})`);
                         break;
                     }
                 }
             }
         } else { // Spawning something
-            spawnCommander.showSpawnInfo(spawn)
+            showSpawnInfo(spawn);
         }
     }
 }
